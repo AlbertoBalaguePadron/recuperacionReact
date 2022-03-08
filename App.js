2d9d58472d4fabe913c0e8273c2114ac629a23b2
Alberto <alberto01bp@gmail.com>
2022-03-08 11:17:21 +0000

    Comienzo del proyecto

---
+++ App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { View, StyleSheet } from "react-native";
+export default function App() {
+  
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flex: 2,
+    height: "50%",
+  }
+});
